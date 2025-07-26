4d1c3a614397bfdd3cf0a26df337936256feb9a9
AlexN09 <alexaloxnok@gmail.com>
2025-07-26 21:54:33 +0200

    ball movement and wall collision

--- src/ball.py
+++ src/ball.py
@@ -1,14 +1,31 @@
 import pygame
-
+from settings import *
+from math import sin, cos, radians  
 class Ball:
-    def __init__(self,_isAttached, _x,_y,_angle,):
+    def __init__(self,_isAttached, _x,_y,_angle,_speed):
             self.isAttached = _isAttached
             self.x = _x
             self.y = _y 
+            self.speed = _speed
             self.angle = _angle
             self.surface = pygame.Surface((20,20))
             self.surface.fill((255,255,255))
     def draw(self,screen):
-        self.x -= 1 
-        self.x -= 1
+        # print(f"current y: {self.y}")
+        print(f"current angle: {self.angle}")
+        if(self.x <= 0 or self.x + 20 >= SCREEN_WIDTH):
+          self.reflect("vertical")
+        elif(self.y <=0 or self.y + 20 >= SCREEN_HEIGHT):
+          self.reflect("horizontal")  
+         
+        #  print("за границой")
+        self.x += self.speed * cos(radians(self.angle))
+        self.y += -self.speed * sin(radians(self.angle))
         screen.blit(self.surface, (self.x,self.y))
+    def reflect(self,collision_type):
+         if(collision_type == "vertical"):
+             self.angle = 180 - self.angle
+         elif(collision_type == "horizontal"):
+             self.angle = -self.angle    
+         
+         
--- src/game.py
+++ src/game.py
@@ -8,12 +8,16 @@ class Game:
         self.clock = _clock
 
     def run(self):
-     ball = Ball(False,100,100,50)
+     ball = Ball(False,400,300,BALL_ANGLE, BALL_SPEED)
      while self.running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False  # ← БЕЗ ":="
-        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("⬅ Нажал влево")
+                ball.reflect()
+           
         self.scr.fill((0, 0, 0))
         ball.draw(self.scr)
 
